ed570346290a9e31b84c5b137e2d4920d7401a42
computri <33151710+computri@users.noreply.github.com>
2017-11-30 12:00:06 +0100

    Add files via upload
    fastest horse in the race

--- my_driver.py
+++ my_driver.py
@@ -4,6 +4,7 @@ import numpy as np
 from network import Network
 import logging
 import helper_functions
+from freestyle import do_the_thing
 
 import math
 
@@ -14,8 +15,11 @@ from pytocl.controller import CompositeController, ProportionalController, \
 
 class MyDriver(Driver):
     def __init__(self, logdata=True):
+        # filenames = ['aalborg.csv', 'alpine-1.csv', 'f-speedway.csv', 'data_track_2.csv']
         filenames = ['aalborg.csv', 'alpine-1.csv', 'f-speedway.csv']
+        # filenames = ['data_track_2.csv']
         data, labels = helper_functions.readData(filenames)
+        # self.network = do_the_thing(data, labels)
         learning_rate = 1e-5
         self.network = Network(data, labels, learning_rate)
         self.network.train()
@@ -30,6 +34,7 @@ class MyDriver(Driver):
         )
         self.data_logger = DataLogWriter() if logdata else None
 
+
     def stateToArray(self, carstate):
         # print([carstate.angle, carstate.current_lap_time, carstate.damage,
         #                     carstate.distance_from_start, carstate.distance_raced,
@@ -40,27 +45,31 @@ class MyDriver(Driver):
         #                     carstate.wheel_velocities, carstate.z,
         #                     carstate.focused_distances_from_edge])
 
-        return np.array([[carstate.distance_from_center, carstate.angle] +
+        return np.array([[carstate.speed_x, carstate.distance_from_center, carstate.angle] +
                             list(carstate.distances_from_edge)])
 
     # Override the `drive` method to create your own driver
     def drive(self, carstate: State) -> Command:
         """
         Produces driving command in response to newly received car state.
-
         This is a dummy driving routine, very dumb and not really considering a
         lot of inputs. But it will get the car (if not disturbed by other
         drivers) successfully driven along the race track.
         """
         command = Command()
         stateList = self.stateToArray(carstate)
+        # output = self.network.activate(stateList)
         output = self.network.forward(stateList).data
         self.steer(carstate, output[0, 2], command)
 
-        # ACC_LATERAL_MAX = 6400 * 5
-        # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
-        v_x = output[0, 3]
-        self.accelerate(carstate, v_x, command)
+        
+        if output[0,0]>0.5:
+            ACC_LATERAL_MAX = 6400 * 5
+            v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+        else:
+            v_x = 0    
+            
+        self.accelerate(carstate, output[0,0], command)
 
         if self.data_logger:
             self.data_logger.log(carstate, command)
